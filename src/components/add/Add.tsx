f67041734e61fa65aad3c3f510e166534b8d9207
Thet Paing Soe <51513681+puyitt@users.noreply.github.com>
2023-08-16 10:55:27 +0630

    first upload

---
+++ src/components/add/Add.tsx
@@ -0,0 +1,50 @@
+import { GridColDef } from "@mui/x-data-grid";
+import './add.scss'
+import { useQueryClient,useMutation } from "react-query";
+
+type Props = {
+    slug:string,
+    columns:GridColDef[],
+    setOpen:React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const Add = (props:Props) => {
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation({
+        mutationFn:()=>{
+            return fetch(`http://localhost:8800/api/${props.slug}s`,{
+                method: "post",
+            });
+        },
+        onSuccess:()=>{
+            queryClient.invalidateQueries([`all${props.slug}s`])
+        }
+    });
+
+    const handleSubmit = (e:React.FormEvent<HTMLFormElement>)=> {
+        e.preventDefault();
+        mutation.mutate()
+    }
+    return (
+        <div className="add">
+            <div className="modal">
+                <span className="close" onClick={()=>props.setOpen(false)}>X</span>
+                <h1>Add New User</h1>
+                <form onSubmit={handleSubmit}>
+                    {props.columns
+                    .filter((item) => item.field !== "id" && item.field !== "img")
+                    .map((column) => (
+                    <div className="item">
+                        <label>{column.headerName}</label>
+                        <input type={column.type} placeholder={column.field} />
+                    </div>
+                    ))}
+                <button>Send</button>
+                </form>
+            </div> 
+        </div>
+    )
+}
+
+export default Add
